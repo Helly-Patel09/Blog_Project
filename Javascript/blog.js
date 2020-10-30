bf3029ce4c1e274e5af53b3f28edd9d4f0191c81
Helly-Patel09 <hellypatel909@gmail.com>
2020-10-29 23:48:17 -0700

    Home,Blog,ContactUS Commit

---
+++ Javascript/blog.js
@@ -0,0 +1,27 @@
+function addblog(){
+    
+    var title = document.getElementById('title').value;
+    var desc = document.getElementById('description').value;
+    var titletext = document.createTextNode(title);
+    var desctext = document.createTextNode(desc);
+    var li = document.createElement('li');
+    var h5 = document.createElement('h5');
+    h5.classList.add('card-title')
+    h5.classList.add('title')
+    var p = document.createElement('p');
+    p.classList.add('card-text');
+    p.classList.add('desc')
+    h5.appendChild(titletext);
+    p.appendChild(desctext);
+    li.appendChild(h5);
+    li.appendChild(p);
+    if(title=='' || desc=='')
+        alert("You must write Something..!!");
+    else{
+        document.getElementById("myList").appendChild(li);
+        document.getElementById('title').value = '';
+        document.getElementById('description').value = '';
+    }
+    //alert(title+" "+desc); 
+
+}
---
+++ Javascript/contact.js
@@ -0,0 +1,68 @@
+function printError(element, msg){
+    document.getElementById(element).innerHTML = msg;
+}
+function validateForm(){
+
+    var name = document.contactForm.name.value;
+    var email = document.contactForm.email.value;
+    var subject = document.contactForm.subject.value;
+    var message = document.contactForm.message.value;
+    
+    var nameErr = true;
+    //validate name
+    if(name==""){
+        printError("nameErr","Please enter your name");
+    }
+    else{
+        var regex = /^[a-zA-Z\s]+$/;
+        if(regex.test(name) === false){
+            printError("nameErr","Please Enter a valid name");
+        }
+        else{
+            printError("nameErr","");
+            nameErr=false;
+        }
+    }
+
+    var emailErr = true;
+    //validate email
+    if(email==""){
+        printError("emailErr","Please enter your Email");
+    }
+    else{
+        var regex = /^\S+@\S+\.\S+$/;
+        if(regex.test(email) === false){
+            printError("emailErr","Please Enter a valid Email");
+        }
+        else{
+            printError("emailErr","");
+            emailErr=false;
+        }
+    }
+
+    var subjectErr = true;
+    //validate subject
+    if(subject==""){
+        printError("subjectErr","Please Enter Subject");
+    }
+    else{
+        printError("subjectErr","");
+        subjectErr=false;
+    }
+
+    var messageErr = true;
+    //validate message
+    if(message == 'Select'){
+        printError("messageErr","Please Enter Details");
+    }
+    else{
+        printError("messageErr","");
+        messageErr=false;
+    }
+
+    if(nameErr || emailErr || subjectErr || messageErr == true){
+        return false;
+    } else {
+        alert("you have submitted the form..")
+    }
+}
